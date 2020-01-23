a287402a1c6419e5ff1b403403ea73d2efc86f36
rafazingano <rafazingano@gmail.com>
2020-01-23 16:24:08 -0300

    Update all

--- src/Views/components/comments.blade.php
+++ src/Views/components/comments.blade.php
@@ -1,10 +1,10 @@
-<div class="kt-portlet">
-    <div class="kt-portlet__head" id="comments">
-        <div class="kt-portlet__head-label">
-            <h3 class="kt-portlet__head-title">{{ $title }}</h3>
+<div class="portlet">
+    <div class="portlet__head" id="comments">
+        <div class="portlet__head-label">
+            <h3 class="portlet__head-title">{{ $title }}</h3>
         </div>
     </div>
-    <div class="kt-portlet__body">
+    <div class="portlet__body">
         {!! Form::open(['route' => [$route, $id], 'method' => 'post', 'class' => 'horizontal-form']) !!}
         <div class="form-group row">
             <div class="col-10">
@@ -16,29 +16,28 @@
         </div>
         <hr>
         {!! Form::close() !!}
-        <div class="kt-widget3">
+        <div class="widget3">
             @isset($comments)
             @foreach($comments as $comment)
-                <div class="kt-widget3__item">
-                    <div class="kt-widget3__header">
-                        <div class="kt-widget3__user-img">
-                            <img class="kt-widget3__img" src="{{ $comment->user->avatar() }}"
-                                 alt="{{ $comment->user->name }} ">
+                <div class="widget3__item">
+                    <div class="widget3__header">
+                        <div class="widget3__user-img">
+                            <img class="widget3__img" src="{{ $comment->user->avatar() }}" alt="{{ $comment->user->name }} ">
                         </div>
-                        <div class="kt-widget3__info">
-                            <a href="#" class="kt-widget3__username">
+                        <div class="widget3__info">
+                            <a href="#" class="widget3__username">
                                 {{ $comment->user->name }} ({{ $comment->user->roles->implode('display_name', ', ') }})
                             </a>
                             <br>
-                            <span class="kt-widget3__time">
+                            <span class="widget3__time">
                                 {{ $comment->created_at->diffForHumans() }}
                             </span>
                         </div>
-                        <span class="kt-widget3__status kt-font-warning">
+                        <span class="widget3__status font-warning">
                         </span>
                     </div>
-                    <div class="kt-widget3__body">
-                        <p class="kt-widget3__text">
+                    <div class="widget3__body">
+                        <p class="widget3__text">
                             {{ $comment->content }}
                         </p>
                     </div>
